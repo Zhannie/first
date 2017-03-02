9160402439a2db3a230b292219822b9f2a01e2ca
Zhanna <Zhanna@Vladislavs-MacBook-Pro.local>
2017-03-02 17:24:46 +0200

    menu.

--- Folio/script.js
+++ Folio/script.js
@@ -1,8 +1,15 @@
+
+
 function myFunction() {
-	var x = document.getElementById("myNav");
+ var x = document.getElementById("myNav");
   if (x.className === "myNav") {
     x.className = "responsive";
   } else {
     x.className ="myNav";
-  }
+  };
 }
+
+
+
+
+
