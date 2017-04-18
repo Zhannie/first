f2d806c5d9d7cc1c1a7c4d85407840d98ee78758
Zhanna <Zhanna@Zhannas-MacBook-Pro.local>
2017-04-18 19:07:36 +0300

    grid update.

--- Pin_joy/script.js
+++ Pin_joy/script.js
@@ -9,37 +9,7 @@ function menuClick(){
 	}
 };
 
-var x = document.querySelectorAll("div.row div.mini");
-var z = document.querySelector(".morebutton");
 
-z.addEventListener('click', collapseIt);
-
-
-function collapseIt() {
-	if (z.textContent === "Хочу больше значков!") {
-		showAll();
-	} else {
-		hideIt();
-	}
-
-};
-
-function showAll() {
-	for (var i = 0; i < x.length; i++) {
-		x[i].classList.remove("col-md-1", "col-sm-2", "col-xs-2");
-		x[i].classList.add("col-md-3", "col-sm-4", "col-xs-6", "full");
-		z.innerHTML = "Свернуть";
-	}
-};
-
-	
-function hideIt() {
-		for (var i = 0; i < x.length; i++) {
-		x[i].classList.remove("col-md-3", "col-sm-4", "col-xs-6", "full");
-		x[i].classList.add("col-md-1", "col-sm-2", "col-xs-2");
-		z.innerHTML = "Хочу больше значков!";
-		}
-};
 
 
 
