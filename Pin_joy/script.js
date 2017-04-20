5425a84495f4cdaf09e2b817a0d0c7c7e7e5c632
Zhanna <Zhanna@Zhannas-MacBook-Pro.local>
2017-04-20 18:25:15 +0300

    last update.

--- Pin_joy/script.js
+++ Pin_joy/script.js
@@ -12,7 +12,28 @@ function menuClick(){
 var z = document.querySelectorAll("a.hex");
 for (var i = 0; i < z.length; i++) {
 	z[i].addEventListener('click', function(){});
-}
+};
+
+$(function() {
+	$(window).scroll(function() {
+		var top = $(window).scrollTop();
+		if (top < 100)
+			$('#hideMe').stop().animate({'opacity':'1'}, 100);
+		else
+			$('#hideMe').stop().animate({'opacity':'.7'}, 100);
+	});
+});
+
+	$('#hideMe').hover (
+		function (e) {
+			var top = $(window).scrollTop();
+			if(top != 0) {
+				$('#hideMe').stop().animate({'opacity':'1'}, 200);
+			}
+		});
+		
+		
+
 
 
 
